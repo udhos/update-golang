dd7d34bf917e48fd7a337ec77c4aced2be166bf4
Everton Marques <everton.marques@gmail.com>
2017-04-08 21:17:24 -0300

    Auto-detect OS and ARCH.

--- update-golang.sh
+++ update-golang.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
 # defaults
+source=https://storage.googleapis.com/golang
 destination=/usr/local
 release=1.8.1
-os=linux
-arch=amd64
 
+os=`uname -s | tr [:upper:] [:lower:]`
+
+case `uname -m` in
+    i*)
+	arch=386
+	;;
+    *)
+        arch=amd64
+	;;
+esac
+
+[ -n "$SOURCE" ] && source=$SOURCE
 [ -n "$DESTINATION" ] && destination=$DESTINATION
 [ -n "$RELEASE" ] && release=$RELEASE
 [ -n "$OS" ] && os=$OS
 [ -n "$ARCH" ] && arch=$ARCH
 
+cat <<EOF
+SOURCE=$source
+DESTINATION=$destination
+RELEASE=$release
+OS=$os
+ARCH=$arch
+EOF
+
 label=go$release.$os-$arch
 filename=$label.tar.gz
-url=https://storage.googleapis.com/golang/$filename
-
-[ -n "$URL" ] && url=$URL
+url=$source/$filename
+goroot=$destination/go
 
 me=`basename $0`
 
@@ -23,38 +41,64 @@ msg() {
     echo >&2 $me: $*
 }
 
+save_dir=$PWD
+
+cleanup() {
+    [ -n "$save_dir" ] && cd $save_dir
+}
+
 die() {
     msg $*
+    cleanup
     exit 1
 }
 
-download() {
-    if [ -r $url ]; then
-	msg $url is local
-	cp $url . || die could not copy from: $url
+solve() {
+    path=$1
+    if echo $path | egrep -q ^/; then
+	echo $path
     else
+	echo $save_dir/$path
+    fi
+}
+
+download() {
+    if echo $url | egrep -q '^https?:'; then
 	msg $url is remote
-	wget $url || die could not download from: $url
+	wget $url || die could not download using wget from: $url
+    else
+	u=`solve $url`
+	msg $u is local
+	cp $u . || die could not copy from: $u
     fi
 }
 
 remove_old_link() {
-    [ -r go ] && rm go
-    [ -r go ] && die could not remove existing golang directory: $destination/go
+    g=`solve $goroot`
+    msg remove old link: $g
+    [ -r $g ] && rm $g
+    [ -r $g ] && die could not remove existing golang directory: $g
 }
 
 untar() {
-    rm -rf $label
-    tar xf $filename || die could not untar: $filename
-    rm $filename
+    l=`solve $destination/$label`
+    msg untar: rm -rf $l
+    rm -rf $l
+    f=`solve $destination/$filename`
+    msg untar: tar xf $f
+    tar xf $f || die could not untar: $f
+    msg untar: rm $f
+    rm $f
 }
 
 relink() {
-    mv go $label
-    ln -s $label go
+    l=`solve $destination/$label`
+    g=`solve $goroot`
+    mv $g $l
+    ln -s $l $g
 }
 
-msg will install golang $label as: $destination/go
+msg will install golang $label as: $goroot
 
 cd $destination || die could not enter destination=$destination
 
@@ -63,5 +107,7 @@ remove_old_link
 untar
 relink
 
-msg golang $label installed at: $destination/go
-msg add $destination/go/bin to your PATH
+msg golang $label installed at: $goroot
+msg remember to add $goroot/bin to your PATH
+
+cleanup
