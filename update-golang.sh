1a1271e0fe8c746be1ea081db95c168fdbf3cad3
Everton Marques <everton.marques@gmail.com>
2017-04-11 01:25:03 -0300

    Clean-up.

--- update-golang.sh
+++ update-golang.sh
@@ -17,6 +17,7 @@ msg version $version
 source=https://storage.googleapis.com/golang
 destination=/usr/local
 release=1.8.1
+profiled=/etc/profile.d/golang_path.sh
 
 os=`uname -s | tr [:upper:] [:lower:]`
 
@@ -35,6 +36,7 @@ esac
 [ -n "$RELEASE" ] && release=$RELEASE
 [ -n "$OS" ] && os=$OS
 [ -n "$ARCH" ] && arch=$ARCH
+[ -n "$PROFILED" ] && profiled=$PROFILED
 
 cat >&2 <<EOF
 SOURCE=$source
@@ -42,6 +44,7 @@ DESTINATION=$destination
 RELEASE=$release
 OS=$os
 ARCH=$arch
+PROFILED=$profiled
 EOF
 
 label=go$release.$os-$arch
@@ -71,60 +74,60 @@ solve() {
     fi
 }
 
+abs_filepath=`solve $filepath`
+abs_url=`solve $url`
+abs_goroot=`solve $goroot`
+abs_new_install=`solve $new_install`
+abs_gobin=$abs_goroot/bin
+abs_gotool=$abs_gobin/go
+abs_profiled=`solve $profiled`
+
 download() {
     if echo $url | egrep -q '^https?:'; then
 	msg $url is remote
-	local f=`solve $filepath`
-	if [ -f "$f" ]; then
-	    msg no need to download - file cached: $f
+	if [ -f "$abs_filepath" ]; then
+	    msg no need to download - file cached: $abs_filepath
 	else
 	    wget -O $filename $url || die could not download using wget from: $url
 	fi
     else
-	local u=`solve $url`
-	msg $u is local
-	cp $u . || die could not copy from: $u
+	msg $abs_url is local
+	cp $abs_url . || die could not copy from: $abs_url
     fi
 }
 
 remove_old_link() {
-    g=`solve $goroot`
-    msg remove old link: $g
-    [ -r $g ] && rm $g
-    [ -r $g ] && die could not remove existing golang directory: $g
+    msg remove old link: $abs_goroot
+    [ -r $abs_goroot ] && rm $abs_goroot
+    [ -r $abs_goroot ] && die could not remove existing golang directory: $abs_goroot
 }
 
 untar() {
-    local l=`solve $new_install`
-    msg untar: rm -rf $l
-    rm -rf $l
-    local f=`solve $filepath`
-    msg untar: tar xf $f
-    tar xf $f || die could not untar: $f
+    msg untar: rm -rf $abs_new_install
+    rm -rf $abs_new_install
+    msg untar: tar xf $abs_filepath
+    tar xf $abs_filepath || die could not untar: $abs_filepath
 }
 
 relink() {
     local l=`solve $new_install`
     local g=`solve $goroot`
-    mv $g $l
-    ln -s $l $g
+    mv $abs_goroot $abs_new_install
+    ln -s $abs_new_install $abs_goroot
 }
 
 path() {
-    local b=`solve $goroot/bin`
-    local p=/etc/profile.d/golang_path.sh
-    msg issuing path $b to $p
-    echo "export PATH=\$PATH:$b" > $p
-    if [ "$b" != /usr/local/go/bin ]; then
-	msg setting up custom GOROOT=$b to $p
-	echo "export GOROOT=$b" >> $p
+    msg issuing path $abs_gobin to $abs_profiled
+    echo "export PATH=\$PATH:$abs_gobin" > $abs_profiled
+    if [ "$abs_gobin" != /usr/local/go/bin ]; then
+	msg setting up custom GOROOT=$abs_goroot to $abs_profiled
+	echo "export GOROOT=$abs_goroot" >> $abs_profiled
     fi
 }
 
 test() {
-    local gotool=`solve $goroot/bin/go`
-    msg testing $gotool:
-    if $gotool version; then
+    msg testing $abs_gotool:
+    if $abs_gotool version; then
 	msg SUCCESS
     else
 	msg FAIL
@@ -143,14 +146,13 @@ symlink_get() {
 # main section: begin
 #
 
-msg will install golang $label as: `solve $goroot`
+msg will install golang $label as: $abs_goroot
 
 cd $destination || die could not enter destination=$destination
 
 download
-g=`solve $goroot`
-if symlink_test $g; then
-    old_install=`symlink_get $g`
+if symlink_test $abs_goroot; then
+    old_install=`symlink_get $abs_goroot`
     msg found symlink for old install: $old_install
 else
     msg not found symlink for old install
@@ -159,8 +161,7 @@ remove_old_link
 untar
 relink
 if [ -n "$old_install" ]; then
-    n=`solve $new_install`
-    if [ "$old_install" != "$n" ]; then
+    if [ "$old_install" != "$abs_new_install" ]; then
 	# remove only install only if it actually changed
 	msg removing old install: $old_install
 	rm -r $old_install
@@ -168,7 +169,7 @@ if [ -n "$old_install" ]; then
 fi
 path
 
-msg golang $label installed at: `solve $goroot`
+msg golang $label installed at: $abs_goroot
 
 test
 cleanup
