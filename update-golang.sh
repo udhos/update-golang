c6b616c7aaf5ac05726c40bfdcbbe5fabe9fa4af
udhos <everton.marques@gmail.com>
2018-03-29 17:31:57 -0300

    Release defaults to 1.10.1

--- update-golang.sh
+++ update-golang.sh
@@ -28,7 +28,7 @@ log_stdin() {
 # defaults
 source=https://storage.googleapis.com/golang
 destination=/usr/local
-release=1.10
+release=1.10.1
 profiled=/etc/profile.d/golang_path.sh
 arch_probe="uname -m"
 
