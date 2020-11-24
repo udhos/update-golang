5032acf176d78ae928847d76a0eeeb2bc960c4cd
Aidyn Torgayev <aidyn.torgayev@gmail.com>
2020-11-24 04:39:38 +0300

    delete archive only if successful untar

--- update-golang.sh
+++ update-golang.sh
@@ -96,8 +96,10 @@ has_cmd() {
 tmp='' ;# will be set
 save_dir=$PWD
 previous_install='' ;# will be set
+tar_to_remove='' ;# will be set
 cleanup() {
     [ -n "$tmp" ] && [ -f "$tmp" ] && msg cleanup: $tmp && rm $tmp
+    [ -n "$tar_to_remove" ] && [ -f "$tar_to_remove" ] && msg cleanup: $tar_to_remove && rm $tar_to_remove
     [ -n "$save_dir" ] && cd "$save_dir" || exit 2
     [ -n "$previous_install" ] && msg remember to delete previous install saved as: "$previous_install"
 }
@@ -261,6 +263,7 @@ untar() {
     local cmd="tar -x -f $abs_filepath"
     msg untar: "$cmd"
     $cmd || die untar: failed: "$abs_filepath"
+    tar_to_remove="$abs_filepath"
 }
 
 relink() {
