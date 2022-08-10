5c2ad1202aa73bb0f387a2ca663c80bda63c93b9
udhos <udhos>
2022-08-09 23:49:38 -0300

    Withstand missing sha256sum tool.

--- sha256-update.sh
+++ sha256-update.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+sum=sha256sum
 out=update-golang.sh.sha256
 
-sha256sum update-golang.sh > $out
+if hash $sum 2>/dev/null; then
+    $sum update-golang.sh > $out
+else
+    echo >&2 $0: missing $sum
+fi
 
 echo $out
